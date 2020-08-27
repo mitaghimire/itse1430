c59c303b77372a4c1ee373eec44788764d3bfd46
16823 <16823@DESKTOP-7LIOJ5N>
2020-08-26 20:55:05 -0500

    playing around with debugging

--- classwork/HelloWorld/HelloWorld/HelloWorld/Program.cs
+++ classwork/HelloWorld/HelloWorld/HelloWorld/Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿/*
+ * Mita Ghimire
+ * ITSE 1430
+ * Lab 1
+ */
+using System;
 
 namespace HelloWorld
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main ( string[] args )
         {
+            // A single line comment
+            // Another line of comment
             Console.WriteLine("Hello World!");
+
+            int hours = 40;
+            double payRate = 12.80;
+
+                double totalpay = hours * payRate;
+            // Another block
+            //10 + 45;
+            
         }
+
     }
-}
+}     
+    
+
+
