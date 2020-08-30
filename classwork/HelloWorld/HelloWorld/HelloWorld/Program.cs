9824478489ba3cb606f322fb70e0672a4a9c411b
16823 <16823@DESKTOP-7LIOJ5N>
2020-08-30 18:44:37 -0500

    playing around with C#

--- classwork/HelloWorld/HelloWorld/HelloWorld/Program.cs
+++ classwork/HelloWorld/HelloWorld/HelloWorld/Program.cs
@@ -7,42 +7,43 @@ using System;
 
 namespace HelloWorld
 {
-    // pascal casting - capitalize on word boundaries including first word
-    // camel casting - capitalize on word boundaries except first ( e.g. firstName, payRate)
+    // pascal casing - capitalize on word boundaries including first word
+    // camel casing - capitalize on word boundaries except first ( e.g. firstName, payRate)
     class Program
     { 
-        // Fuction declaration - declaration to compile that a function exists with a given signature
+        // Fuction declaration - declaration to compiler that a function exists with a given signature
         // Fuction signature - fuction name and the parameter types (sometime it will include return type)
         // Function defination - declaration + implementation
-        static void Main ( string[] args )
+        static void Main ( string[] args ) //parameter
         {
             // A single line comment
-            // Another line of comment
+            // Another line of comments
 
             // display given string to output
-            Console.WriteLine("Hello World!");
+            //Arguments - data passed to function
+            Console.WriteLine("Hello World!"); //printf, cout
 
-            int hours = 40;
-            double payRate = 12.80;
-
-            double totalPay = hours * payRate;
-              
-            // variale declaration
+            // variale declaration: 
             // T id; 
+            //int hours;
+            //hours = 10; //Assignment operator id =E
 
-            //always camel case
-
-            //prefered : T id = E;
-           
-            
-            hours = 10; // assigment operator : id = E
-
-           
+            // Identifier Rules
+            //1. unique within scope
+            //2. must satrt with underscore or letter
+            //3. consist of letters, digits and underscores
+            //always camel case local variable(noun) and parameter
+            //Preferred: T id = E;
+            int hours = 10;
 
             //int pay = 0;
-            // pay = hours *9;
-            int pay = hours *9;
-       
+            //pay = hours * 9;
+            int totalpay = hours * 9;
+
+            //Function overloading - multiple functions with same name but different paremeters
+            //atof, atoi
+            Console.WriteLine(totalpay);
+      
         }
 
     }
