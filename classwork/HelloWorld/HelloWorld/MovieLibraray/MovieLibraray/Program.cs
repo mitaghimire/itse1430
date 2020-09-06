110dd188b767e29a1410fcb7f26afab4118a92e3
16823 <16823@DESKTOP-7LIOJ5N>
2020-09-06 17:34:06 -0500

    working with Movie Library

---
+++ classwork/HelloWorld/HelloWorld/MovieLibraray/MovieLibraray/Program.cs
@@ -0,0 +1,64 @@
+﻿/*
+ * ITSE 1430
+ * movie Library Console Application
+ */
+    using System;
+
+namespace MovieLibraray
+{
+    class Program
+    {
+        static void Main ( string[] args )
+        {
+            //FunWithTYpes();
+            //FunWithVariable();
+
+            //while = while (E) S;
+            //0+ iterations, pre test conditon
+            while (true)
+            {
+                //scope - lifetime of a variable : starts at declaration and continues untile end of current scope
+                char choice = DisplayMenu();
+                if (choice == 'Q')
+                    return;
+            };
+
+            DisplayMenu();
+
+            string title = "";
+            string description = "";
+            string rating = "";
+            int duration;
+
+            static void DisplayMenu ()
+            {
+                do
+                {
+
+
+
+                    Console.WriteLine("Movie Library");
+                    Console.Write("...........");
+
+                    Console.WriteLine("Q)uit");
+
+                    //Get input from user
+                    string input = Console.ReadLine();
+
+                    //c++ : if (x =10);
+                    // if (E) S;
+                    if (input =="Q") // 2 equal sign = equality
+                        return 'Q';
+
+
+
+                    Console.WriteLine("Invalid option");
+                } while (true);
+
+
+
+
+            }
+        }
+    }
+}
