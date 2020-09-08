d093a0cfa7217e57a922ed488ffe4beb3c3a840a
16823 <16823@DESKTOP-7LIOJ5N>
2020-09-07 20:55:03 -0500

    fun with movie

--- classwork/HelloWorld/HelloWorld/MovieLibraray/MovieLibraray/Program.cs
+++ classwork/HelloWorld/HelloWorld/MovieLibraray/MovieLibraray/Program.cs
@@ -13,52 +13,48 @@ namespace MovieLibraray
             //FunWithTYpes();
             //FunWithVariable();
 
-            //while = while (E) S;
-            //0+ iterations, pre test conditon
+
+            // while => while (E) S ;
+            // 0+ iterations, pre test condition
             while (true)
             {
-                //scope - lifetime of a variable : starts at declaration and continues untile end of current scope
                 char choice = DisplayMenu();
                 if (choice == 'Q')
                     return;
             };
 
-            DisplayMenu();
-
             string title = "";
             string description = "";
             string rating = "";
             int duration;
-
-            static void DisplayMenu ()
-            {
-                do
-                {
-
-
-
-                    Console.WriteLine("Movie Library");
-                    Console.Write("...........");
-
-                    Console.WriteLine("Q)uit");
-
-                    //Get input from user
-                    string input = Console.ReadLine();
-
-                    //c++ : if (x =10);
-                    // if (E) S;
-                    if (input =="Q") // 2 equal sign = equality
-                        return 'Q';
-
-
-
-                    Console.WriteLine("Invalid option");
-                } while (true);
-
-
-
-
-            }
         }
+
+         static char DisplayMenu ()
+         {
+            // 1+ iteration, post test
+            // do S while (E);
+            // block statement => { S* }
+            do
+            {
+
+                Console.WriteLine("MovieLibrary");
+                Console.WriteLine("............");
+
+                Console.WriteLine("Q)uit");
+
+                 // Get input from user
+                string input = Console.ReadLine();
+
+                 // C++: if (X = 10 ) ;
+                //If (E) S;
+                if (input == "Q") // 2 equal signs => equality
+                  return 'Q';
+
+              Console.WriteLine("Invalid option");
+            } while (true);
+
+
+         }
     }
+
 }
