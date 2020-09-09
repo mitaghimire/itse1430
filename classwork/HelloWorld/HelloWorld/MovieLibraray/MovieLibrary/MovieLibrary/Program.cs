84f5eb4edb7c19b5cf26d7dcef41ce8240a74849
16823 <16823@DESKTOP-7LIOJ5N>
2020-09-08 22:06:36 -0500

    fun with movie

--- classwork/HelloWorld/HelloWorld/MovieLibraray/MovieLibrary/MovieLibrary/Program.cs
+++ classwork/HelloWorld/HelloWorld/MovieLibraray/MovieLibrary/MovieLibrary/Program.cs
@@ -22,7 +22,7 @@ namespace MovieLibrary
                 if (choice == 'Q')
                     return;
                 else if (choice == 'A')
-                    AddMenu();
+                    AddMovie();
             };
 
             string title = "";
@@ -32,24 +32,28 @@ namespace MovieLibrary
             
         }
 
-        static void AddMenu ()
+        static void AddMovie ()
         {
             // Get title
             Console.WriteLine("Movie title: ");
-            string title = Console.ReadLine();
+            //string title = Console.ReadLine();
+            string title = ReadString(true);
 
             // Get description
-            Console.WriteLine("Description: ");
-            string description = Console.ReadLine();
+            Console.WriteLine("Description (optional): ");
+            //string description = Console.ReadLine();
+            string description = ReadString(false);
 
             // Get rating
             Console.WriteLine("Rating: ");
-            string rating = Console.ReadLine();
+            //string rating = Console.ReadLine();
+            string rating = ReadString(false);
 
 
             // Get duration
             Console.WriteLine("Duration (in minuate): ");
-            string duration = Console.ReadLine();
+            //string duration = Console.ReadLine();
+            int duration = ReadInt32(0);
 
             // Get is classic
             Console.WriteLine("Is Classic (Y/N)? ");
@@ -80,19 +84,82 @@ namespace MovieLibrary
                 else if (value == "A")
                     return 'A';
 
-                DisplayError();
+                DisplayError("Invalid option");
             } while (true);
             
         }
 
-        private static void DisplayError ()
+        private static void DisplayError ( string message)
         {
             Console.BackgroundColor = ConsoleColor.Red;
             Console.ForegroundColor = ConsoleColor.White;
                 
-            Console.WriteLine("Invalid option");
+            Console.WriteLine(message);
 
             Console.ResetColor();
         }
+        static int ReadInt32()
+        {
+            return ReadInt32(Int32.MinValue);
+        }
+        static int ReadInt32( int minimumValue )
+        {
+            do
+            {
+                string value = Console.ReadLine();
+
+                //Parse to int int32.Parse (string) -not safe
+                // int result = Int32.Parse(value);
+
+                //Parameter kinds
+                //  Input parameter ("pass by value") - a copy of the argument is places into parameter inside function defination
+                //  Input/output parameter ("pass by reference") - a reference to the argument is passed to the function and both orginal argument and parameter reference same value (C++: int& )
+                // Output parameter -function caller does not provide input, function always provides output ( C++ : return type)
+                //bool Int32.TryParse (string, out int result )
+                // Double.Parse/TryParse
+                //Single.Parse/TryParse
+                //Boolean.Parse/TryParse
+                //Int16.Parse/TryParse
+                int result;
+              if (Int32.TryParse(value, out result) && result >= minimumValue)
+                return result;
+
+                if (minimumValue != Int32.MinValue)   //Int32.MaxValue
+                    DisplayError("Value must be at least" + minimumValue); //string concatenation
+                else
+                DisplayError("Must be integral value");
+            } while (true);
+        }
+        //Logical operators (booleans)
+        // Not = > !E
+        // AND => E && E : boolean
+        // OR => E || E : boolean
+
+        //Relational operators ( primitives + afew extra )
+        // equality => E == E
+        // inequality => E != E
+        // greater than  => E > E
+        // greater than or equal to => E >= E
+        // less than => E < E
+        // less than or equal to => E <= E
+
+        static string ReadString ( bool required )
+        {
+            do
+            {
+                string value = Console.ReadLine();
+
+                //if not reuired or not empty return
+                if (!required || value != "")
+                        return value;
+                //{
+                    // Is it empty
+                    //if (value == "")
+                        DisplayError("value is required");
+                //}
+               // return value;
+            } while (true);
+    }
+
     }
 }
