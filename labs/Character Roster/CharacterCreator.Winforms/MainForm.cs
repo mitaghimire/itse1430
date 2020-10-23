569873659e2008188a41016265cb9929b3302f0b
Mita Ghimire <mita.ghimire@my.tccd.edu>
2020-10-23 15:11:12 -0500

    lab 3 not finished yet

---
+++ labs/Character Roster/CharacterCreator.Winforms/MainForm.cs
@@ -0,0 +1,84 @@
+﻿/*
+ * ITSE 1430
+ * Mita Ghimire
+ * Lab3
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CharacterCreator.Winforms
+{
+    public partial class MainForm : Form
+    {
+        ICharacterRoster roster = new Roster();
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void tsMenuItemExit_Click(object sender, System.EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void tsMenuItemAbout_Click(object sender, EventArgs e)
+        {
+            About abt = new About();
+            abt.ShowDialog();
+        }
+
+        private void tsMenuCharacterNew_Click(object sender, EventArgs e)
+        
+        {
+            CharacterNew chrNew = new CharacterNew();
+            chrNew.ShowDialog();
+            BindListBox();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {           
+            BindListBox();
+        }
+
+        private void BindListBox()
+        {
+
+            BindingSource bs = new BindingSource();
+            bs.DataSource = roster.GetAll();
+            lstCharacter.DataSource = bs;
+            lstCharacter.DisplayMember = "Name";
+            lstCharacter.Refresh();
+            lstCharacter.Update();
+        }
+
+        private void tsMenuCharacterEdit_Click(object sender, EventArgs e)
+        {
+            CharacterEdit chrEdit = new CharacterEdit(lstCharacter.SelectedValue);
+            chrEdit.ShowDialog();
+            BindListBox();
+        }
+
+        private void tsMenuCharacterDelete_Click(object sender, EventArgs e)
+        {
+            DialogResult confirmation = MessageBox.Show($"Are you sure to delete {((Character)lstCharacter.SelectedValue).Name}?", "Deletion", MessageBoxButtons.YesNo);
+            if(confirmation == DialogResult.Yes)
+            {
+                roster.Delete(((Character)lstCharacter.SelectedValue).UniqueIdentifier);
+                BindListBox();
+            }
+
+        }
+
+        private void lstCharacter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ labs/Character Roster/CharacterCreator/ICharacterRoster.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * ITSE 1430
+ * Mita Ghimire
+ * Lab3
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CharacterCreator
+{
+    public interface ICharacterRoster
+    {
+        Character CreateCharacter(string name, string profession, string race, Attributes attribute, string description);
+
+
+        int Add(Character addCharacter);
+        void Delete(int uniqueIdentifier);
+
+        Character Get(int uniqueIdentifier);
+        List<Character> GetAll();
+
+        void Update(int uniqueIdentifier,Character updateCharacter);
+    }
+}
